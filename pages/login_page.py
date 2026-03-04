b8a8112a6618a23e64442d5c0c8680c45ca32e50
Aleksey Ivanov <aleksey.ivanov0@ya.ru>
2026-03-04 23:49:28 +0300

    add login page tests

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        print(self.browser.current_url)
+        assert 'login' in self.browser.current_url, 'Url not contains "login"'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), 'Registration form is not present'
+    
---
+++ test_login_page.py
@@ -0,0 +1,19 @@
+from pages.login_page import LoginPage
+
+
+LINK = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+
+def test_login_page_url(browser):
+    page = LoginPage(browser, LINK)
+    page.open()
+    page.should_be_login_url()
+
+def test_login_form(browser):
+    page = LoginPage(browser, LINK)
+    page.open()
+    page.should_be_login_form()
+
+def test_registration_form(browser):
+    page = LoginPage(browser, LINK)
+    page.open()
+    page.should_be_register_form()
